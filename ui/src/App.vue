c88dff2aec31136a3861266264114916e5a2a1ac
Willi <kalle83@email.de>
2022-01-16 17:12:56 +0100

    (ui): add app bar

--- ui/src/App.vue
+++ ui/src/App.vue
@@ -1,7 +1,10 @@
 <template>
   <v-app>
-    
-    <v-main class="pt-8">
+    <v-app-bar color="deep-purple" dark app>
+      <v-app-bar-title>Feedback Carousel</v-app-bar-title>
+      <v-spacer/>
+    </v-app-bar>
+    <v-main class="pt-20">
       <Question />
     </v-main>
   </v-app>
